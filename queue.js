e1b90ba363bb55be2b7110bf00cc72950e2c37a3
SabineSafi <sabine.safi@gmail.com>
2016-11-09 11:43:03 +0100

    Store data in local Storage

--- queue.js
+++ queue.js
@@ -1,9 +1,21 @@
-let latestAttribution = 0
-let currentWaitingList = []
+localStorage.setItem('latestAttribution', 0)
+localStorage.setItem('currentWaitingList', JSON.stringify([]))
+
+const currentWaitingList = JSON.parse(localStorage.getItem('currentWaitingList'))
+
+const growAttributionCount = () => {
+	const currentAttribution = JSON.parse(localStorage.getItem('latestAttribution')) +1
+	localStorage.setItem('latestAttribution', currentAttribution)
+}
+
+const growWaitingList = (latestAttribution) => {
+	currentWaitingList.push(latestAttribution)
+	localStorage.setItem('currentWaitingList', JSON.stringify(currentWaitingList))
+}
 
 const introduceWaitingList = () => {
 	currentWaitingList.forEach(attribution => {
-		if (attribution < latestAttribution) {
+		if (attribution < localStorage.getItem('latestAttribution')) {
 			document.querySelector("#display-waiting-list")
 			.innerHTML = `Les numéros suivants sont avant vous dans la liste d'attente :
 				<br><ul id="waiting-list"></ul>`
@@ -13,7 +25,7 @@ const introduceWaitingList = () => {
 
 const generateWaitingList = () => {
 	currentWaitingList.forEach(attribution => {
-		if (attribution < latestAttribution) {
+		if (attribution < localStorage.getItem('latestAttribution')) {
 			const li = document.createElement('li')
 			li.innerHTML = `Numéro ${attribution}`
 			document.querySelector("#waiting-list").appendChild(li)
@@ -22,10 +34,10 @@ const generateWaitingList = () => {
 }
 
 const newAttribution = () => {
-	latestAttribution++
-	currentWaitingList.push(latestAttribution)
+	growAttributionCount()
+	growWaitingList(localStorage.getItem('latestAttribution'))
 	document.querySelector("#display-attribution")
-	.innerHTML = `Vous êtes le numéro : <strong>${latestAttribution}</strong> dans la liste d'attente.`
+	.innerHTML = `Vous êtes le numéro : <strong>${localStorage.getItem('latestAttribution')}</strong> dans la liste d'attente.`
 	introduceWaitingList()
 	generateWaitingList()
 }
