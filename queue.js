d5bb2ae97efe6a9b0d73ac84e123d774e0139046
SabineSafi <sabine.safi@gmail.com>
2016-11-10 16:42:02 +0100

    Added different queueing messages. Added some styling.

--- queue.js
+++ queue.js
@@ -3,6 +3,18 @@ localStorage.setItem('currentWaitingList', JSON.stringify([]))
 
 const currentWaitingList = JSON.parse(localStorage.getItem('currentWaitingList'))
 
+const newAttribution = () => {
+	growAttributionCount()
+	const latestAttribution = localStorage.getItem('latestAttribution')
+	growWaitingList(latestAttribution)
+	document.querySelector("#display-attribution")
+	.innerHTML = `Vous êtes le numéro : <strong>${latestAttribution}</strong> dans la liste.`
+	if (latestAttribution >= 2) {removeDiv()	}
+	introduceWaitingList(latestAttribution)
+	generateWaitingList()
+}
+
+
 const growAttributionCount = () => {
 	const currentAttribution = JSON.parse(localStorage.getItem('latestAttribution')) +1
 	localStorage.setItem('latestAttribution', currentAttribution)
@@ -13,31 +25,38 @@ const growWaitingList = (latestAttribution) => {
 	localStorage.setItem('currentWaitingList', JSON.stringify(currentWaitingList))
 }
 
-const introduceWaitingList = () => {
-	currentWaitingList.forEach(attribution => {
-		if (attribution < localStorage.getItem('latestAttribution')) {
-			document.querySelector("#display-waiting-list")
-			.innerHTML = `Les numéros suivants sont avant vous dans la liste d'attente :
-				<br><ul id="waiting-list"></ul>`
-			}
-		})
+const removeDiv = () => {
+	const childDiv = document.querySelector('#display-waiting-list')
+	const parentDiv = document.querySelector('#bloc-client')
+	parentDiv.removeChild(childDiv)
+}
+
+const introduceWaitingList = (latestAttribution) => {
+	const div = document.createElement('div')
+	div.setAttribute('id', 'display-waiting-list')
+	const generateInnerHTML = (latestAttribution) => {
+		if (latestAttribution <= currentWaitingList.sort()[0]) {
+			return `<p>Vous êtes le prochain sur la liste d'attente !</p>`
+		}
+		else {
+			return `<p>Les numéros suivants sont avant vous :</p>
+				<ul id="waiting-list"></ul>`
+		}
+	}
+	div.innerHTML = generateInnerHTML(latestAttribution)
+	document.querySelector('#bloc-client').appendChild(div)
 }
 
 const generateWaitingList = () => {
 	currentWaitingList.forEach(attribution => {
-		if (attribution < localStorage.getItem('latestAttribution')) {
-			const li = document.createElement('li')
-			li.innerHTML = `Numéro ${attribution}`
-			document.querySelector("#waiting-list").appendChild(li)
+		if (localStorage.getItem('latestAttribution') > 1) {
+			if (attribution < localStorage.getItem('latestAttribution')) {
+				const li = document.createElement('li')
+				li.innerHTML = `Numéro ${attribution}`
+				document.querySelector("#waiting-list").appendChild(li)
+			}
 		}
 	})
 }
 
-const newAttribution = () => {
-	growAttributionCount()
-	growWaitingList(localStorage.getItem('latestAttribution'))
-	document.querySelector("#display-attribution")
-	.innerHTML = `Vous êtes le numéro : <strong>${localStorage.getItem('latestAttribution')}</strong> dans la liste d'attente.`
-	introduceWaitingList()
-	generateWaitingList()
-}
+
